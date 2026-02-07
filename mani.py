8ca1d1b44188a75820569e161c41a2a140658efb
bot-Auj <loxloe14@gmail.com>
2026-02-07 13:21:17 +0300

    Create mani.py

---
+++ mani.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import commands
+import os
+from flask import Flask
+from threading import Thread
+
+# --- كود التشغيل المستمر (Keep Alive) ---
+app = Flask('')
+@app.route('/')
+def home():
+    return "I'm alive"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+# --------------------------------------
+
+# إعداد الصلاحيات (Intents)
+intents = discord.Intents.default()
+intents.message_content = True  # ضروري جداً لقراءة الأوامر
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'البوت شغال الآن باسم: {bot.user}')
+
+@bot.command()
+async def promo(ctx):
+    try:
+        await ctx.message.delete()
+    except:
+        pass
+        
+    content = (
+        "**تم توفير نيتـ؛روهات قيـ؛فت ( شهر )**\n"
+        "** بـ  18,5 ريال  <:70226nitrostylediamond:1461906861497516115>  **\n\n"
+        "للطلب او للإستفسار : [اضغط هنا](https://discord.com/channels/1455993144645648517/1456000073208037427)\n"
+        "لقراءة الشروط والاحكام : [اضغط هنا](https://discord.com/channels/1455993144645648517/1455999275325460502)\n"
+        "-# عند شراءك فـ أنت توافق على الشروط ، تقولي ماقريت مب عذر\n"
+        "** @everyone **"
+    )
+    await ctx.send(content)
+
+# تشغيل السيرفر الوهمي ثم البوت
+keep_alive()
+bot.run(MTQ2NTMzMzg5MTk3MDc2MDkwMw.Gfn-rb.kBGbdhjjT_WgfhKttQuddx9LVi3TCJSw8qb9W0)
